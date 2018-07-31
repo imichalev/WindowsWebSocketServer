512f16a40c25d4397de6d0949db8dd7653d252f1
imichalev <imichalev@gmail.com>
2018-07-31 16:52:53 +0300

    New Code

--- WindowsWebSocket/Program.cs
+++ WindowsWebSocket/Program.cs
@@ -8,15 +8,41 @@ using WindowsWebSocket;
 using System.Data.SqlClient;
 using System.Configuration;
 using System.Data.Common;
-
+using System.Windows.Forms.DataVisualization.Charting;
 namespace WindowsWebSocket
 {
     class Program
     {
-        //public string conn="Data Source=(LocalDB)\v11.0;AttachDbFilename="C:\Users\imihalev\Documents\Visual Studio 2013\WindowsWebSocketServer\WindowsWebSocket\loggerDatabase.mdf";
+        //public string conn="Data Source=(LocalDB)\v11.0;AttachDbFilename=C:\Users\imihalev\Documents\Visual Studio 2013\WindowsWebSocketServer\WindowsWebSocket\loggerDatabase.mdf";
         public static WebSocketServer wsServer;
+
+
+
+
+
+
+
+
         static void Main(string[] args)
         {
+
+            //DateTime time = DateTime.Now;
+            //double unix=Math.Round((time - new DateTime(1970, 1, 1, 0, 0, 0, 0)).TotalSeconds);
+            
+            //Console.WriteLine("Human read time:{0}",time);
+            //Console.WriteLine("Unix:{0}", unix);
+
+            //// Unix timestamp is seconds past epoch
+            ////System.DateTime dtDateTime = new DateTime(1970, 1, 1, 0, 0, 0, 0, System.DateTimeKind.Utc);
+            ////dtDateTime = dtDateTime.AddSeconds(unixTimeStamp).ToLocalTime();
+           
+
+            //Console.WriteLine("Coonnection to Database error.");
+            //Console.ReadLine();
+            //return;
+
+
+
             string provider = ConfigurationManager.AppSettings["provider"];
             string connectionString = ConfigurationManager.AppSettings["connectionString"];
             SqlConnection dbCon = new SqlConnection(connectionString);
@@ -33,10 +59,49 @@ namespace WindowsWebSocket
                     "SELECT COUNT(*) FROM Logger",dbCon);
                 int count = (int)command.ExecuteScalar();
                 Console.WriteLine("I have {0} counts.", count);
+                if (count > 1)
+                {
+                    command = new SqlCommand("SELECT * FROM Logger", dbCon);
+                    SqlDataReader reader = command.ExecuteReader();
+                   
+                    Console.WriteLine();
+                    using (reader)
+                    {
+                        while (reader.Read())
+                        {
+                            int index = (int)reader["Logger_id"];
+                            decimal rtcData = (decimal)reader["rtc"];
+                            decimal tempData = (decimal)reader["temperature"];
+                           
+                            Console.WriteLine("Logger Data:{0} -->{1} {2}", index, rtcData, tempData);
+                        }
+                    }
+
+                    Console.WriteLine();
+                    command = new SqlCommand("SELECT TOP(5) * FROM  Logger ORDER BY Logger_id  DESC", dbCon);
+                    reader = command.ExecuteReader();
+                    
+                    using (reader)
+                    {
+                        while (reader.Read())
+                        {
+                            int index = (int)reader["Logger_id"];
+                            decimal rtcData = (decimal)reader["rtc"];
+                            decimal tempData = (decimal)reader["temperature"];
+                            Console.WriteLine("Logger Last Data:{0} -->{1} {2}", index, rtcData, tempData);
+                        }
+                    }
+
+                    Console.WriteLine();
+
+
+
+                }
             
                 command = dbCon.CreateCommand();
                 command.CommandText="INSERT INTO Logger(rtc,temperature) VALUES(@param1,@param2)";
-                long rtc = DateTime.Now.TimeOfDay.Ticks;
+
+                decimal rtc =(decimal) Math.Round((DateTime.Now - new DateTime(1970, 1, 1, 0, 0, 0, 0)).TotalSeconds);
                 command.Parameters.Add("@param1", System.Data.SqlDbType.Decimal).Value = rtc;
                 command.Parameters.Add("@param2", System.Data.SqlDbType.Float).Value = 30.6;
                 command.ExecuteNonQuery();
